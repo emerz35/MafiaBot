582a4aaecd95a678cbba93af2cbe0d191255c70d
emerz35 <charlie.h.hands@gmail.com>
2018-11-29 19:30:48 +0000

    Fixed url and removed driver initialisation

--- src/main/java/com/mafia/mafiabot/DatabaseManager.java
+++ src/main/java/com/mafia/mafiabot/DatabaseManager.java
@@ -1,7 +1,6 @@
 package com.mafia.mafiabot;
 
 import java.sql.*;
-import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
 
 /**
@@ -9,46 +8,33 @@ import org.javacord.api.entity.user.User;
 * @author Charlie Hands
 */
 public class DatabaseManager {
-    private Connection conn;
-    private final String url = "mysql://" + System.getenv("database-name")+":3306/";
-    private void startConnection() throws ClassNotFoundException, SQLException{
-        Class.forName("com.mysql.jdbc.Driver");
-        conn = DriverManager.getConnection(url, System.getenv("database-user"), System.getenv("database-password"));
-    }
-    private void endConnection()throws SQLException{
-        conn.close();
-    }
+    private final String url = "mysql://playerdatabase:3306/";
+    
     public void addPlayer(User user){
-        try{
-        startConnection();
+        try(Connection conn=DriverManager.getConnection(url,System.getenv("database-username"),System.getenv("database-password"))){
         conn.createStatement().executeUpdate("INSERT INTO players VALUES ("+ user.getDiscriminatedName()+",0)");
-        endConnection();
-        }catch(SQLException|ClassNotFoundException e ){
+        }catch(SQLException e ){
             e.printStackTrace();
         }
     }
     public int getPlayerScore(User user){
-        try{
-        startConnection();
+        try(Connection conn=DriverManager.getConnection(url,System.getenv("database-username"),System.getenv("database-password"))){
         ResultSet set = conn.createStatement().executeQuery("SELECT score FROM players WHERE displayName="+user.getDiscriminatedName()+";");
-        endConnection();
         return set.getInt("score");
-        }catch(SQLException|ClassNotFoundException e ){
+        }catch(SQLException e){
             e.printStackTrace();
         }
         return 0;
     }
     public void createTable(){
-    	try{
-            startConnection();
+    	try(Connection conn=DriverManager.getConnection(url,System.getenv("database-username"),System.getenv("database-password"))){
             conn.createStatement().executeUpdate("DROP TABLE IF EXIST players;"
                     + "CREATE TABLE players ("
 	    			+ "id INTEGER not NULL,"
 	    			+ "displayName VARCHAR(255),"
 	    			+ "score INTEGER not NULL,"
 	    			+ "PRIMARY KEY (id));");
-            endConnection();
-    	}catch(SQLException | ClassNotFoundException e) {
+    	}catch(SQLException e) {
     		e.printStackTrace();
     	}
     	
